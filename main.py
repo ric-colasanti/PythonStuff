73d569f07723fba3fea6f8384d1b1ef70ac3b996
Dr Ric <ricardo.colasanti@glasgow.ac.uk>
2023-08-11 20:57:12 +0100

    mpi test

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
-from modules import agent
-from modules import cell
+'''
+to run:
+ mpirun -n 2 --hosts hp1 python main.py
+'''
+from mpi4py import MPI
+import sys
 
-print("main")
+comm = MPI.COMM_WORLD
+name = MPI.Get_processor_name()
+
+sys.stdout.write("Hello from hp1\n")
+sys.stdout.write(f"Name: {name}  Rank {comm.rank}\n")
